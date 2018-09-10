0151fd3ad251c20830e2cfd6065660ae3fe6c287
BurghardH <burghard.hillig@googlemail.com>
2018-09-10 21:15:11 +0200

    Implemented individual alerts and checks for input values.

--- app/src/main/java/com/companyname/basisfitnesstest/MainActivity.java
+++ app/src/main/java/com/companyname/basisfitnesstest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.companyname.basisfitnesstest;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -56,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("You are :", "Checked");
                 }
                 else {
-                    genderSwitchIsChecked = geschlechtSwitch.isChecked();
+                    genderSwitchIsChecked = !geschlechtSwitch.isChecked();
                     geschlechtTextView.setText("Männlich");  //To change the text near to switch
                     Log.d("You are :", " Not Checked");
                 }
@@ -80,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 String laufTimeString = s.toString();
-                if (!IsAllDigits(laufTimeString))
+                if (!isAllDigits(laufTimeString))
                 {
                     laufTimeEnteredAlert();
                 }
@@ -93,11 +94,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void laufTimeEnteredAlert() {
+    private void laufTimeEnteredAlert()
+    {
+        if (!inputAlertTriggered)
+        {
+            displayAlert("Achtung", "Es dürfen nur Zahlen und ':' eingegeben werden!", "OK");
+        }
+    }
+
+    private void displayAlert(String titleString, String messageString, String buttonString) {
         AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-        alertDialog.setTitle("Achtung");
-        alertDialog.setMessage("Es dürfen nur Zahlen und ':' eingegeben werden!");
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+        alertDialog.setTitle(titleString);
+        alertDialog.setMessage(messageString);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, buttonString,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
@@ -106,12 +115,10 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    public boolean IsAllDigits(String inputString)
+    public boolean isAllDigits(String inputString)
     {
         for (int i=1; i<inputString.length(); i++)
         {
-            /*Log.i("Schleife", "Postiton: " + i);
-            Log.i("Schleife", "Länge: " + inputString.length());*/
             if (!(true == Character.isDigit(inputString.charAt(i))))
             {
                 if (!(inputString.charAt(i) == ':'))
@@ -125,16 +132,84 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
         inputAlertTriggered = false;
-        int alter = Integer.parseInt(alterEditText.getText().toString());
-        double sprintTime = Double.parseDouble(sprintEditText.getText().toString());
-        double klimmhangTime = Double.parseDouble(klimmhangEditText.getText().toString());
-        double laufTime = Double.parseDouble(laufEditText.getText().toString());
+        //int alter = Integer.parseInt(alterEditText.getText().toString());
+        double sprintTime = sprintTimeEntered(sprintEditText);
+        //double klimmhangTime = KlimmhangTimeEntered(klimmhangEditText);
+        //double laufTime = LaufTimeEntered(laufEditText);
         //BFTErgebnisBerechnung Auswertung = new BFTErgebnisBerechnung(genderSwitchIsChecked, alter, sprintTime, klimmhangTime, laufTime);
+        //if (!inputAlertTriggered)
+        //{
+            //await Navigation.PushAsync(new ViewResult(Auswertung));
+        //}
+    }
+
+    private double sprintTimeEntered(EditText inputEditText){
+//        double sprintTime = Double.parseDouble(inputEditText.getText().toString());
+        double validTime = checkForValidTime(inputEditText);
+        if (validTime <= 0)
+        {
+            inputEditText.setBackgroundColor(Color.RED);
+            displayMinTimeInputAlert();
+        }
+        return validTime;
+    }
+
+    private double checkForValidTime(EditText inputEditText)
+    {
+        double validTime = 0.0;
+        if (!isInteger(inputEditText.getText().toString()))
+        {
+            inputEditText.setBackgroundColor(Color.RED);
+            displayWrongInputAlert();
+        }
+        else
+        {
+            inputEditText.setBackgroundColor(Color.TRANSPARENT);
+            validTime = Double.parseDouble(inputEditText.getText().toString());
+        }
+        return validTime;
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        // only got here if we didn't return false
+        return true;
+    }
+
+    private void displayWrongInputAlert()
+    {
         if (!inputAlertTriggered)
         {
-            //await Navigation.PushAsync(new ViewResult(Auswertung));
+            displayAlert("Achtung", "Der Wert wurde nicht als Zahl eingegeben!", "OK");
+            inputAlertTriggered = true;
         }
+    }
 
+    private void displayMinTimeInputAlert()
+    {
+        if (!inputAlertTriggered)
+        {
+            displayAlert("Achtung", "Zeiten müssen größer als Null sein!", "OK");
+            inputAlertTriggered = true;
+        }
+    }
+
+    private double KlimmhangTimeEntered(EditText inputEditText){
+        double klimmhangTime = Double.parseDouble(inputEditText.getText().toString());
+
+        return 0;
+    }
+
+    private double LaufTimeEntered(EditText inputEditText){
+        double laufTime = Double.parseDouble(inputEditText.getText().toString());
+
+        return 0;
     }
 
 
