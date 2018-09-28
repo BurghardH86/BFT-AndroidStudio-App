f35144eab2d3b58ead6102c5a60b7314496f8ba6
BurghardH <burghard.hillig@googlemail.com>
2018-09-28 07:45:55 +0200

    Implemented a working splash screen

--- app/src/main/java/com/companyname/basisfitnesstest/SplashScreen.java
+++ app/src/main/java/com/companyname/basisfitnesstest/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.companyname.basisfitnesstest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,23 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread activityThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(mainActivityIntent);
+                    finish();
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        activityThread.start();
 
     }
 }
